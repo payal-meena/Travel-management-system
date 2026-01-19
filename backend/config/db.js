baf33ca3adba086a60100952ba2382a80223fddf
Kshama-Meena <kshamameena534@gmail.com>
2026-01-19 12:02:43 +0530

    connect with atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -19,7 +19,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB Atlas Connected");
+    console.log("MongoDB Atlas Connected.....");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
